52c0881d9235a645cbc110fa76d0ea9592b395d3
Andrea Soares <asoaresdev@gmail.com>
2023-05-11 20:53:45 -0300

    feat:exercicios

---
+++ exercicio1/script.js
@@ -0,0 +1,24 @@
+const nome = prompt("Qual seu nome?")
+const comidaFavorita1 = prompt("Qual sua comida favorita?")
+const comidaFavorita2 = prompt("Qual sua segunda comida favorita?")
+const comidaFavorita3 = prompt("Qual sua terceira comida favorita?")
+
+console.log(`Estas são as comidas favoritas de ${nome}:
+- ${comidaFavorita1}
+- ${comidaFavorita2}
+- ${comidaFavorita3}`);
+
+
+
+
+
+// ================ USANDO FUNCÇÃO ===================
+
+// const imprimeComidasFavortias = (nome, comida1, comida2, comida3) =>{
+//     console.log(`Estas são as comidas favoritas de ${nome}:
+//     - ${comida1}
+//     - ${comida2}
+//     - ${comida3}`);
+// }
+
+// imprimeComidasFavortias(nome, comidaFavorita1, comidaFavorita2, comidaFavorita3)
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,18 @@
 //Crie a const para a frase aqui
+const frase = "Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair\""
+
+// a mesma frase usando template string 
+const fraseComTemplateString = `Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS,  ${"mas não deixe o gato sair".toUpperCase()}"`
+
+
+const newString = frase.replace("verde", "rosa").replace("azul","laranja")
+
+console.log(newString);
+
+const fraseUsandoReplace = newString.replace("mas não deixe o gato sair","MAS NÃO DEIXE O GATO SAIR")
+
+console.log(fraseUsandoReplace);
+
+const fraseUsandoToUpperCase = `Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, ${"mas não deixe o gato sair".toUpperCase()}"`
+
+console.log(fraseComTemplateString);
